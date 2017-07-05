2382864b7749fbe62ae05cc9044b6877180020b4
Shivam sharma <shivams334@gmail.com>
2017-07-06 00:10:22 +0530

    Update models.py

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(object):
+	"""helps django work with our custom user model"""
+	def create_user(self,email,name,password=None):
+		if not email:
+			raise ValueError('User must have email')
+		email = self.normalize_email(email)
+		user = self.model(email=email, name=name)
+
+		user.set_password(password)
+	
+		
+
+
+
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionMixin):
+
+	"""docstring for UserProfile"""
+	email = models.EmailField(max_length=255, unique=True)
+	name = models.CharField(max_length=255)
+	is_active = models.BooleanField(default=True)
+	is_staff = models.BooleanField(default=False)
+
+	object = UserProfileManager()
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['name']
+
+	def get_full_name(self):
+		"""used to get a user full name"""
+		return self.name
+
+	def get_short_name():
+		"""used to get a users short name"""
+		return self.name
+
+	def __str__(self):
+		"""Django uses this when it needs to convert the object into string"""
+		return self.email
+		
