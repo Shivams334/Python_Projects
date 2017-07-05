94e39b9bf0347cf055920acc9c812007e8492169
Shivam sharma <shivams334@gmail.com>
2017-07-06 00:15:24 +0530

    Update models.py

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -12,15 +12,16 @@ class UserProfileManager(object):
 		user = self.model(email=email, name=name)
 
 		user.set_password(password)
-	
+		user.save(using=self._db)
+
+		return user
+	def create_superuser(self,email,name,password):
+		"""creates and saves a new superuser with given details"""
+		user = self.creates_user(email,name,password)
+		user.is_superuser = True
+		user.is_staff = True
+		user.save(using=self._db)	
 		
-
-
-
-
-
-
-
 class UserProfile(AbstractBaseUser, PermissionMixin):
 
 	"""docstring for UserProfile"""
