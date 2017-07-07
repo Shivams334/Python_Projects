84e48bf6511cab273a7a0c439842385e5e9926a4
Shivam sharma <shivams334@gmail.com>
2017-07-08 01:54:15 +0530

    Update views.py
    Added serializer view along with standard HTTP response (404)

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
-from rest_framework.views import APIView 
+
+from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from . import serializers
+from rest_framework import status
+
 # Create your views here.
 
 class HelloApiView(APIView):
-	def get(self,request,format=None):
-		an_apiview=[
-			'Uses HTTP methods as function (get, post, patch, put)',
- 			'It is similar to a traditional Djnago view',
- 			'Gives you the most control over the logic',
- 			'Is mapped manually to URLs',
- 		]
-		return Response({'message': 'hello!', 'an_apiview': an_apiview})
+    """Test API View."""
+    
+    serializer_class = serializers.HelloSerializer
 
+    def get(self, request, format=None):
+        """Returns a list of APIView features."""
+
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'It is similar to a traditional Django view',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+      
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+serializer.errors, status=status.HTTP_400_BAD_REQUEST)
