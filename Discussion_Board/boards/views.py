ea526d503133254353190738318e270007bfe9df
Shivam sharma <shivams334@gmail.com>
2017-12-10 18:46:58 +0530

    Update views.py

--- Discussion_Board/boards/views.py
+++ Discussion_Board/boards/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Board
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 # Create your views here.
 
@@ -16,4 +18,21 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'new_topic.html', {'board': board})
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
